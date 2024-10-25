615df3c26d8e5ea5d3ca04ea84ecaedeadd4a078
DimaShapoval <dsapoval422@gmail.com>
2024-10-25 12:29:24 +0300

    add initialValue

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -9,9 +9,9 @@ export default function HomePage() {
   useEffect(() => {
     if (allProducts) {
       const { data } = allProducts;
-      setCard(
-        data.map((items) => {
-          if(items.images.length >= 3){
+      const newArrayOfProducts = data
+        .map((items) => {
+          if (items.images.length >= 3) {
             return (
               <Cards
                 key={items.id}
@@ -22,19 +22,41 @@ export default function HomePage() {
                 id={items.id}
               />
             );
+          } else {
+            return undefined;
           }
-          else{
-            return null;
-          }
-          
         })
-      );
+        .filter((item) => item !== undefined);
+      if (newArrayOfProducts.length === 0) {
+        let publicLink = process.env.PUBLIC_URL;
+        let initialItem = {
+          id: 11111,
+          title: 'Headphones Level',
+          category: {
+            name: "Electronics",
+          },
+          description:
+            "Level Headphones offer an exceptional audio experience with premium sound quality, designed for comfort and extended wear. Equipped with advanced noise-canceling technology, these headphones allow you to immerse yourself fully in your favorite music or podcasts without distraction. The sleek, modern design features durable materials, a comfortable headband, and soft ear cushions, ensuring a snug fit for hours of listening. With high-fidelity sound, deep bass, and clear treble, Level Headphones provide an outstanding audio experience, whether you’re working, traveling, or relaxing. Stay connected on the go with built-in Bluetooth and intuitive touch controls, making it easy to adjust volume, change tracks, and take calls with ease.",
+          images: [`${publicLink}/productImages/1.jpg`, `${publicLink}/productImages/2.jpg`, `${publicLink}/productImages/3.jpg`],
+          price: 50
+        };
+        newArrayOfProducts.push(<Cards
+          key={initialItem.id}
+          title={initialItem.title}
+          description={initialItem.description}
+          price={initialItem.price}
+          image={initialItem.images[0]}
+          id={initialItem.id}
+        />)
+      }
+
+      setCard(newArrayOfProducts);
     }
   }, [allProducts]);
   return (
     <Grid
       columns={"repeat(auto-fit, minmax(300px, 350px)"}
-      justify={'center'}
+      justify={"center"}
       gap={"6"}
       m={"5"}
       width="auto"
--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -8,13 +8,31 @@ import ProductInfoSection from "../../components/ProductInfoSection/ProductInfoS
 export default function ProductPage() {
   const [productInfo, setProductInfo] = useState(false);
   const { id } = useParams();
+  const publicLink = process.env.PUBLIC_URL
 
   useEffect(() => {
     const getProductInfo = async () => {
-      const { data } = await axios.get(
+      try{
+        const { data } = await axios.get(
         `https://api.escuelajs.co/api/v1/products/${id}`
       );
       setProductInfo(data);
+      }
+      catch(err){
+        let initialItem = {
+          id: 11111,
+          title: 'Headphones Level',
+          category: {
+            name: "Electronics",
+          },
+          description:
+            "Level Headphones offer an exceptional audio experience with premium sound quality, designed for comfort and extended wear. Equipped with advanced noise-canceling technology, these headphones allow you to immerse yourself fully in your favorite music or podcasts without distraction. The sleek, modern design features durable materials, a comfortable headband, and soft ear cushions, ensuring a snug fit for hours of listening. With high-fidelity sound, deep bass, and clear treble, Level Headphones provide an outstanding audio experience, whether you’re working, traveling, or relaxing. Stay connected on the go with built-in Bluetooth and intuitive touch controls, making it easy to adjust volume, change tracks, and take calls with ease.",
+          images: [`${publicLink}/productImages/1.jpg`, `${publicLink}/productImages/2.jpg`, `${publicLink}/productImages/3.jpg`],
+          price: 50
+        };
+        setProductInfo(initialItem);
+      }
+      
     };
     getProductInfo();
   }, [id]);
